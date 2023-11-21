cfc6dee906a426728a0503cc367c2d5c187a065c
Nick <nicolas.cinera@gmail.com>
2023-11-21 18:14:33 -0500

    Rec bin search on the go

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -21,3 +21,20 @@ export default function bs_list(haystack: number[], needle: number): boolean {
         }
     }
 }
+
+export function bs_list_recursive(haystack: number[], needle: number): boolean {
+    if (haystack.length === 0) {
+        return false;
+    }
+
+    const mid = Math.floor(haystack.length / 2);
+    const midValue = haystack[mid];
+
+    if (midValue === needle) {
+        return true;
+    } else if (needle < midValue) {
+        return bs_list(haystack.slice(0, mid), needle);
+    } else {
+        return bs_list(haystack.slice(mid), needle);
+    }
+}
