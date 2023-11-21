8d51e0e2b8e1962d3760ee060d96a9cec1e2ab60
Nick <nicolas.cinera@gmail.com>
2023-11-21 18:03:28 -0500

    Bin Search on the go lol

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
+// Binary Search on top of my head, my recursive approach reached max rec level (probably did something wrong)
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let mid = Math.floor(haystack.length / 2);
+    let midValue = haystack[mid];
 
+    while (true) {
+        if (haystack.length === 1) {
+            return haystack[0] === needle;
+        }
+
+        if (midValue === needle) {
+            return true;
+        } else if (needle < midValue) {
+            haystack = haystack.slice(0, mid);
+            mid = Math.floor(haystack.length / 2);
+            midValue = haystack[mid];
+        } else {
+            haystack = haystack.slice(mid);
+            mid = Math.floor(haystack.length / 2);
+            midValue = haystack[mid];
+        }
+    }
 }
