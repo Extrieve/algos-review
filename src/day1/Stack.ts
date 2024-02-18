447d5462fdf44cd8cef1b92eac9f8b45f32be1ef
Extrieve <nicolas.cinera@gmail.com>
2024-02-17 21:53:04 -0500

    Refactor Stack class to use a linked list implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,35 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  public head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    if (!this.head) {
+      this.head = { value: item };
     }
+    this.head = { value: item, next: this.head };
+    this.length++;
+  }
 
-    push(item: T): void {
+  pop(): T | undefined {
+    if (!this.head) return undefined;
 
-}
-    pop(): T | undefined {
+    const { value } = this.head;
+    this.head = this.head.next;
 
-}
-    peek(): T | undefined {
-
-}
+    this.length--;
+    return value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
