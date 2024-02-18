a2a52ec3b648e8c730de046f71a0ec5f9c931eee
Extrieve <nicolas.cinera@gmail.com>
2024-02-17 21:06:38 -0500

    Refactor Queue class to include enqueue, deque, and peek methods

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,38 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  public head?: Node<T>;
+  public tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item };
+    if (this.tail) {
+      this.tail.next = node;
     }
+    this.tail = node;
+    if (!this.head) {
+      this.head = node;
+    }
+    this.length++;
+  }
+  deque(): T | undefined {
+    if (!this.head) return undefined;
 
-    enqueue(item: T): void {
-
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+    return value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
